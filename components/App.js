0d4f3ffe4afa64a378ed20fc8486554fac04de33
Jim Burch <jrburch1@gmail.com>
2021-04-12 01:40:48 -0700

    setup next and navbar

---
+++ components/App.js
@@ -0,0 +1,13 @@
+import Layout from './Layout';
+
+const App = () => {
+  return (
+    <Layout>
+      <div>
+        <h1>F1 Survivor</h1>
+      </div>
+    </Layout>
+  );
+};
+
+export default App;
---
+++ components/Nav.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const Nav = () => {
+
+  return (
+    <div>
+      <MenuIcon />
+      <h1>Nav</h1>
+    </div>
+  );
+};
+
+export default Nav;
